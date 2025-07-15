e84922c68de51d5e16d9d7a746321a257336bf36
ivannofick <ivannofick@gmail.com>
2025-07-15 20:07:58 +0700

    bismillah

--- packages/create-teapotapps/app/src/routes/api.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import WelcomeController from '../controllers/WelcomeController.js'
-const router = Router()
-
-router.get('/', WelcomeController.wellcome)
-
-export default router
