8110c38224f4a85173cb271216949db5b460645a
ivannofick <ivannofick@gmail.com>
2026-05-20 22:19:18 +0700

    refactor: remove global controller response wrapper in favor of direct RestApiHandler usage

--- packages/teapotapps/app/src/routes/api.js
+++ packages/teapotapps/app/src/routes/api.js
@@ -3,12 +3,9 @@ const router = Router()
 import * as WelcomeController from '../controllers/WelcomeController.js'
 import { verifyToken } from '../middlewares/verifyToken.js'
 import { routeGroup } from '../core/routeGroup.js'
-import { controller } from '../core/http.js'
-
-const Welcome = controller(WelcomeController)
 
 routeGroup(router, "/api", [verifyToken], (api) => {
-    api.get('/wellcome', Welcome.apiWellcome)
+    api.get('/wellcome', WelcomeController.apiWellcome)
 });
 
 export default router
