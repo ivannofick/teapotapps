43d1f33b7e2058241b489eb26568c2e3e801b6b6
ivannofick <ivannofick@gmail.com>
2026-05-20 22:26:49 +0700

    feat: implement RestApiHandler and JwtHandlers, and refactor controller response logic to use manual response handling

--- packages/app/src/controllers/WelcomeController.js
+++ packages/app/src/controllers/WelcomeController.js
@@ -1,17 +1,11 @@
 import { render } from "../core/http.js";
+import { responseApi } from "../libs/RestApiHandler.js";
 
 export const wellcome = async (req, res) => {
   return await render(res, 'Wellcome', { name: 'Ivannofick' });
 };
 
 export const apiWellcome = async (req, res) => {
-  return {
-    data: { 'users': 1 },
-    meta: { "saya": 1 },
-    status: {
-      code: 0,
-      message_client: "Users fetched successfully",
-    },
-  };
+  return responseApi(res, { 'users': 1 }, { "saya": 1 }, "Users fetched successfully");
 };
 
